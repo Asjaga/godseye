6c13ea7c238297ffcff0149db84ee07d479b1532
Aakash <Aakashsinghjaga@gmail.com>
2026-02-07 10:36:23 +0530

    all commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,15 @@ dotenv.config();
 const app = express();
 
 /* ------------------ MIDDLEWARE ------------------ */
-app.use(express.json());
+app.use(express.json({
+  limit: "15mb"
+}));
+
+app.use(express.urlencoded({
+  limit: "15mb",
+  extended: true
+}));
+
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
